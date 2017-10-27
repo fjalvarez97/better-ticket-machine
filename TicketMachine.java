1155bf5bd84962c4bd9b71d72f26d83837f9cec8
fjalvarez97 <franjalvarez955@gmail.com>
2017-10-27 11:47:10 +0200

    He añadido un parametro booleano, en caso de que sea true se imprime un ticket adicional

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,13 +17,16 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    // The kind of machine
+    private boolean prize;
 
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int cost, boolean special)
     {
         price = cost;
+        prize = special;
         balance = 0;
         total = 0;
     }
@@ -67,7 +70,18 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        if(balance >= price) {
+        if(balance >= price) 
+        {
+            
+                if (prize == true)
+                {
+                    //Simulate the printing of a prize ticket
+                    System.out.println("##################");
+                    System.out.println("# The BlueJ Line");
+                    System.out.println("# Prize Ticket");
+                    System.out.println("##################");
+                    System.out.println();
+                }
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -75,7 +89,7 @@ public class TicketMachine
             System.out.println("# " + price + " cents.");
             System.out.println("##################");
             System.out.println();
-
+             
             // Update the total collected with the price.
             total = total + price;
             // Reduce the balance by the prince.
